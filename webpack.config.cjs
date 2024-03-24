b7ac37e2a643b6e2b3f1878ef09750724f67fdcd
Charleston Anjos <charleston.anjos@gmail.com>
2024-03-24 19:06:54 -0300

    add webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      model: path.resolve(__dirname, 'src/model/'),
+      services: path.resolve(__dirname, 'src/definitions/services/')
+    }
+  },
+  optimization: {
+    minimize: false
+  },
+  performance: {
+    hints: 'warning',
+    maxAssetSize: 500 * 1024,
+    maxEntrypointSize: 500 * 1024
+  },
+  devtool: 'source-map'
+}
